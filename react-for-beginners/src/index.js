d1a97bc2686f4f0834176d446a54455b9aaa3d26
gnuoynawh <bizxhy@gmail.com>
2022-08-03 12:24:17 +0900

    #6 EFFECTS

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+/*
+  todo - 아래 형식은 17버전 (참고 : https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis)
+
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import App from './App';
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+*/
